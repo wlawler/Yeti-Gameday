36fdbd8d76e0d0653ff73e297348a01058fc4d70
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-10-31 19:54:58 -0400

    added home and marketplace routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { MarketPlaceComponent} from './market-place/market-place.component'; 
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "home-component", component: HomeComponent}, 
+  {path: "marketPlace-component", component: MarketPlaceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
