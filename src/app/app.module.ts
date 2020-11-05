654c1c14bc4955105e7a7b382c209b419a89197c
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-11-04 22:13:27 -0500

    added product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MarketPlaceComponent } from './market-place/market-place.component';
+import { ProductListComponent } from './market-place/Product-List/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MarketPlaceComponent
+    MarketPlaceComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
