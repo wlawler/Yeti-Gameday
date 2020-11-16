290cba91bd58c2382a1b2f2ce2e8816d57954f3a
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-11-15 19:28:31 -0500

    working on notify button

--- src/app/market-place/Product-List/product-list/product-list.component.ts
+++ src/app/market-place/Product-List/product-list/product-list.component.ts
@@ -12,6 +12,9 @@ products = products
 share() {
   window.alert('The product has been shared');
 }
+onNotify() {
+  window.alert('You will be notified when the product goes on sale')
+}
   constructor() { }
 
   ngOnInit(): void {
